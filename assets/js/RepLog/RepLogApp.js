15d1d6eb54c80d2c98e408b9e0868c32e23d192a
Ma, Kyryll <kyryll.ma@mail.int>
2018-10-02 16:34:35 +0300

    add some interactivity

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -2,10 +2,27 @@ import {Component} from "react";
 import React from "react";
 
 export default class RepLogApp extends Component {
+
+    constructor(props) {
+        super(props);//parent constructor
+        this.state = {highlightedRowId: null};
+    }
+
+    handleMouseMove(repLogId, event) {
+        this.setState({highlightedRowId: repLogId});
+    }
+
+    handleMouseLeave(event) {
+        this.setState({highlightedRowId: null});
+    }
+
     render() {
+
+        const { highlightedRowId } = this.state;
+        const { withHeart } = this.props;
         let heart = '';
-        if (this.props.withHeart) {
-            heart = <span>❤️</span>;
+        if (withHeart) {
+            heart = <span>♥️️</span>;
         }
         const repLogs = [
             { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
@@ -38,7 +55,12 @@ export default class RepLogApp extends Component {
                         {
                             repLogs.map((repLog) => {
                                 return (
-                                    <tr key={repLog.id}>
+                                    <tr
+                                        key={repLog.id}
+                                        className={highlightedRowId === repLog.id ? 'info' : ''}
+                                        onMouseMove={(event) => this.handleMouseMove(repLog.id, event) }
+                                        onMouseLeave={(event) => this.handleMouseLeave(event) }
+                                    >
                                         <td>{repLog.itemLabel}</td>
                                         <td>{repLog.reps}</td>
                                         <td>{repLog.totalWeightLifted}</td>
@@ -57,6 +79,37 @@ export default class RepLogApp extends Component {
                     </tr>
                     </tfoot>
                 </table>
+
+                <form className="form-inline">
+                    <div className="form-group">
+                        <label className="sr-only control-label required" htmlFor="rep_log_item">
+                            What did you lift?
+                        </label>
+                        <select id="rep_log_item"
+                                name="item"
+                                required="required"
+                                className="form-control">
+                            <option value="">What did you lift?</option>
+                            <option value="cat">Cat</option>
+                            <option value="fat_cat">Big Fat Cat</option>
+                            <option value="laptop">My Laptop</option>
+                            <option value="coffee_cup">Coffee Cup</option>
+                        </select>
+                    </div>
+                    {' '}
+                    <div className="form-group">
+                        <label className="sr-only control-label required" htmlFor="rep_log_reps">
+                            How many times?
+                        </label>
+                        <input type="number" id="rep_log_reps"
+                               name="reps" required="required"
+                               placeholder="How many times?"
+                               className="form-control"/>
+                    </div>
+                    {' '}
+                    <button type="submit" className="btn btn-primary">I Lifted it!</button>
+                </form>
+
             </div>
         );
     }
