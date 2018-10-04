3eceddc2c58b7a913a992465056453918f0c123c
Ma, Kyryll <kyryll.ma@mail.int>
2018-10-04 16:10:32 +0300

    I don't get paid enough for this shit.

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,6 +1,7 @@
 import {Component} from "react";
 import React from "react";
-import RepLogList from './RepLogList';
+import PropTypes from 'prop-types';
+import RepLogs from './RepLogs';
 
 
 export default class RepLogApp extends Component {
@@ -10,7 +11,6 @@ export default class RepLogApp extends Component {
         this.state = {highlightedRowId: null};
         this.handleMouseMove = this.handleMouseMove.bind(this);
         this.handleMouseLeave = this.handleMouseLeave.bind(this);
-
     }
 
     handleMouseMove(repLogId) {
@@ -22,71 +22,21 @@ export default class RepLogApp extends Component {
     }
 
     render() {
-
         const { highlightedRowId } = this.state;
         const { withHeart } = this.props;
-        let heart = '';
-        if (withHeart) {
-            heart = <span>♥️️</span>;
-        }
+
         return (
-            <div className="col-md-7">
-                <h2>Lift Stuff! {heart}</h2>
-                <table className="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>What</th>
-                        <th>How many times?</th>
-                        <th>Weight</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                    </thead>
-                        <RepLogList
-                            highlightedRowId={highlightedRowId}
-                            onMouseMove={this.handleMouseMove}
-                            onMouseLeave={this.handleMouseLeave}
-                        />
-                    <tfoot>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <th>Total</th>
-                        <th>TODO</th>
-                        <td>&nbsp;</td>
-                    </tr>
-                    </tfoot>
-                </table>
-
-                <form className="form-inline">
-                    <div className="form-group">
-                        <label className="sr-only control-label required" htmlFor="rep_log_item">
-                            What did you lift?
-                        </label>
-                        <select id="rep_log_item"
-                                name="item"
-                                required="required"
-                                className="form-control">
-                            <option value="">What did you lift?</option>
-                            <option value="cat">Cat</option>
-                            <option value="fat_cat">Big Fat Cat</option>
-                            <option value="laptop">My Laptop</option>
-                            <option value="coffee_cup">Coffee Cup</option>
-                        </select>
-                    </div>
-                    {' '}
-                    <div className="form-group">
-                        <label className="sr-only control-label required" htmlFor="rep_log_reps">
-                            How many times?
-                        </label>
-                        <input type="number" id="rep_log_reps"
-                               name="reps" required="required"
-                               placeholder="How many times?"
-                               className="form-control"/>
-                    </div>
-                    {' '}
-                    <button type="submit" className="btn btn-primary">I Lifted it!</button>
-                </form>
-
-            </div>
+            <RepLogs
+                highlightedRowId={highlightedRowId}
+                withHeart={withHeart}
+                onMouseMove={this.handleMouseMove}
+                onMouseLeave={this.handleMouseLeave}
+            />
         );
     }
 }
+
+
+RepLogApp.propTypes = {
+    withHeart: PropTypes.bool
+};
--- assets/js/RepLog/RepLogList.js
+++ assets/js/RepLog/RepLogList.js
@@ -1,43 +1,43 @@
-import {Component} from "react";
 import React from "react";
+import PropTypes from 'prop-types';
 
 
-export default class RepLogList extends Component {
+export default function RepLogList (props) {
+    const {
+        highlightedRowId,
+        onMouseMove,
+        onMouseLeave
+    } = props;
 
-    render () {
+    const repLogs = [
+        { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
+        { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
+        { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
+    ];
 
-        const {
-            highlightedRowId,
-            onMouseMove,
-            onMouseLeave
-        } = this.props;
-
-        const repLogs = [
-            { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
-            { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
-            { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
-        ];
-
-        return (<tbody>
-        {
-            repLogs.map((repLog) => {
-                return (
-                    <tr
-                        key={repLog.id}
-                        className={highlightedRowId === repLog.id ? 'info' : ''}
-                        onMouseMove={()  => onMouseMove(repLog.id) }
-                        onMouseLeave={() => onMouseLeave() }
-                    >
-                        <td>{repLog.itemLabel}</td>
-                        <td>{repLog.reps}</td>
-                        <td>{repLog.totalWeightLifted}</td>
-                        <td>...</td>
-                    </tr>
-                );
-            })
-        }
-        </tbody>);
+    return (<tbody>
+    {
+        repLogs.map((repLog) => {
+            return (
+                <tr
+                    key={repLog.id}
+                    className={highlightedRowId === repLog.id ? 'info' : ''}
+                    onMouseMove={()  => onMouseMove(repLog.id) }
+                    onMouseLeave={() => onMouseLeave() }
+                >
+                    <td>{repLog.itemLabel}</td>
+                    <td>{repLog.reps}</td>
+                    <td>{repLog.totalWeightLifted}</td>
+                    <td>...</td>
+                </tr>
+            );
+        })
     }
-
-
+    </tbody>);
 }
+
+RepLogList.propTypes = {
+    highlightedRowId: PropTypes.any,
+    onMouseMove: PropTypes.func.isRequired,
+    onMouseLeave: PropTypes.func.isRequired,
+};
